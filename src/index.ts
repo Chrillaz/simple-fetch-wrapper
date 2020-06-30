c4e665d9d20dccc544e34d423687830121c8594a
Christoffer Öhman <totalchriz@gmail.com>
2020-06-30 21:52:15 +0200

    Rollup and a bit more organized code

--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,7 @@
-export interface HttpInstance {
-  baseUrl: string;
-  defaultContentType: string;
-  setBaseUrl: (url: string) => void;
-  getBaseUrl: (extention: string) => string;
-  setDefaultContentType: (type: string) => void;
-  getDefaultContentType: () => string;
-  makeRequest: (request: RequestInit) => Promise<HttpResponseObject>;
-  get: (url: string, args: RequestInit) => Promise<HttpResponseObject>;
-  post: (url: string, args: RequestInit) => Promise<HttpResponseObject>;
-}
-
-export interface RequestInit {
-  url: string;
-  method: string;
-  headers?: Headers | {[key: string]: string};
-  params?: {[key: string]: string};
-  body?: string;
-  cache?: any;
-  mode?: any;
-}
-
-export interface HttpResponseObject {
-  success: boolean; 
-  status: number; 
-  data?: any; 
-  statusText?: string
-}
-
-export type Listeners = {[key: string]: Function[] | (HTTPInterceptor | Function)};
-
-export type HTTPInterceptor = (config: RequestInit) => RequestInit;
-
-interface HttpRequestInit extends RequestInit {};
-
-interface HttpResponse extends HttpResponseObject {}
-
-class HttpResponse {
-
-  constructor (response: Response, json: any) {
-    
-    Object.assign(this, response);
-
-    this.success
-      ? this.data = json
-      : this.statusText = response.statusText ? response.statusText : json.error || '';
-  }
-}
-
-class HttpEmitter {
-
-  public listeners: Listeners = {};
-  
-  public emit (event: string, args: any) {
-
-    return this.listeners[event].length > 0
-      ? (this.listeners[event] as Function[]).forEach(f => f(args))
-      : false;
-  }
-  
-  public interceptor (fn: HTTPInterceptor) {
-
-    return this.listeners.interceptor = fn;
-  }
-
-  public isFetching (fn: (status: boolean) => boolean) {
-
-    return this.listeners.isFetching = fn;
-  }
-
-  public on (event: string, fn: Function) {
-
-    this.listeners[event] = this.listeners[event] || [];
-
-    (this.listeners[event] as Function[]).push(fn);
-
-    return this;
-  }
-
-  public detach (event: string, fn: Function) {
-
-    const index = (this.listeners[event] as Function[]).indexOf(fn);
-      
-    return index > -1
-      ? (this.listeners[event] as Function[]).splice(index, 1)
-      : false;
-  }
-}
-
-class HttpRequestInit {
-
-  constructor (listeners: Listeners, url: string, init?: RequestInit) {
-
-    if (init && listeners.interceptor != undefined) {
-      
-      init = (listeners.interceptor as HTTPInterceptor)(init);
-    }
-
-    Object.assign(this, init);
-
-    if (init && init.headers != undefined) {
-
-      this.headers = new Headers();
-
-      for (const property in init.headers) {
-
-        this.headers.append(property, (init.headers as {[key: string]: string})[property]);
-      }
-    }
-
-    if (init && init.params != undefined) {
-
-      this.url = url + Object.keys(init.params).reduce((acc, curr, index) => 
-        acc + `${index === 0 ? '?' : '&'}${curr}=${'' + (init?.params as {[key: string]: string})[curr]}`, '');
-    }
-
-    if (init && init.body != undefined) {
-
-      this.body = JSON.stringify(init.body);
-    }
-  }
-}
+import { HttpResponse } from './modules/httpresponse'
+import { HttpRequestInit } from './modules/httprequestinit'
+import { HttpEmitter } from './modules/httpemitter'
+import { HTTPResponseObject, HTTPRequestInit, HTTPRequestObject } from './types';
 
 export class Http extends HttpEmitter {
 
@@ -161,13 +43,15 @@ export class Http extends HttpEmitter {
     return this.defaultContentType;
   }
 
-  async makeRequest (request: RequestInit): Promise<HttpResponse> {
+  async makeRequest (request: Omit<HTTPRequestInit, 'params'>): Promise<HTTPResponseObject> {
+    
+    const {url, ...rest} = request as Omit<HTTPRequestInit, 'params'>;
 
     try {
-
-      const response = await fetch(new Request(request.url, request)),
+      
+      const response = await fetch(url, rest),
             json = await response.json();
-            
+
       return JSON.parse(JSON.stringify(new HttpResponse(response, json)));
     } catch (error) {
 
@@ -181,12 +65,12 @@ export class Http extends HttpEmitter {
     } 
   }
 
-  async get (url: string, args: RequestInit): Promise<HttpResponse> {
-
+  async get (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
+    
     return await this.makeRequest(new HttpRequestInit(this.listeners, url, {...args, method: 'GET'}));
   }
 
-  async post (url: string, args: RequestInit): Promise<HttpResponse> {
+  async post (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
   
     return await this.makeRequest(new HttpRequestInit(this.listeners, url, {...args, method: 'POST'}));
   }
