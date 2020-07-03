9ff1fcf99462a70d3223cca21db9ee46973d1fc1
Christoffer Öhman <totalchriz@gmail.com>
2020-07-03 05:03:06 +0200

    patch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,128 @@
-import { HttpResponse } from './modules/httpresponse'
-import { HttpRequestInit } from './modules/httprequestinit'
-import { HttpEmitter } from './modules/httpemitter'
-import { HTTPResponseObject, HTTPRequestInit, HTTPRequestObject } from './types';
+export interface HTTPInstance {
+  baseUrl: string;
+  defaultContentType: string;
+  setBaseUrl: (url: string) => void;
+  getBaseUrl: (extention: string) => string;
+  setDefaultContentType: (type: string) => void;
+  getDefaultContentType: () => string;
+  makeRequest: (request: HTTPRequestInit) => Promise<HTTPResponseObject>;
+  get: (url: string, args?: HTTPRequestObject) => Promise<HTTPResponseObject>;
+  post: (url: string, args?: HTTPRequestObject) => Promise<HTTPResponseObject>;
+}
+
+export interface HTTPRequestObject {
+  method?: string;
+  params?: {[key: string]: any};
+  headers?: {[key: string]: string} | Headers;
+  body?: {[key: string]: any} | string;
+  referrer?: string;
+  cache?: any;
+  mode?: any;
+  credentials?: any;
+}
+
+export interface HTTPRequestInit extends HTTPRequestObject {
+  url: string;
+  headers: Headers;
+  body?: string;
+}
+
+export interface HTTPResponseObject {
+  success: boolean; 
+  status: number; 
+  data?: any; 
+  statusText?: string
+}
+
+export interface HTTPListeners {
+  [key: string]: Function[] | (HTTPListeners['interceptor'] | HTTPListeners['isFetching']);
+  interceptor: (config: HTTPRequestInit) => HttpRequestInit;
+  isFetching: (status: boolean) => boolean;
+}
+
+export interface HttpResponse extends HTTPResponseObject {}
+
+export class HttpResponse {
+
+  constructor (response: Response, json: any) {
+    
+    this.success = response.ok;
+
+    this.status = response.status;
+
+    this.success
+      ? this.data = json
+      : this.statusText = response.statusText ? response.statusText : json.error || '';
+  }
+}
+
+export interface HttpRequestInit extends HTTPRequestInit {};
+
+export class HttpRequestInit {
+  
+  constructor ({interceptor}: HTTPListeners, url: string, init?: HTTPRequestObject) {
+
+    const properties = {headers: new Headers(), url, ...init} as HTTPRequestInit;
+
+    new Promise<HTTPRequestInit>((resolve) => resolve(interceptor != undefined ? interceptor(properties) : properties))
+
+    .then(requestInit => {
+
+      Object.assign(this, requestInit);
+
+      if (init) {
+
+        this.url = this.params ? this.url + Object.keys(this.params).reduce((acc, curr, index) =>
+          acc + `${index === 0 ? '?' : '&'}${curr}=${'' + this.params![curr]}`, '') : this.url;
+
+        this.body = this.body ? JSON.stringify(this.body) : undefined;
+
+        for (const property in this.headers) {
+
+          this.headers[property] = this.headers[property];
+        }
+      }
+    });
+  }
+}
+
+class HttpEmitter {
+
+  public listeners = {} as HTTPListeners;
+  
+  public interceptor (fn: (config: HTTPRequestInit) => HTTPRequestInit) {
+    
+    return this.listeners.interceptor = fn;
+  }
+  
+  public isFetching (fn: (status: boolean) => boolean) {
+    
+    return this.listeners.isFetching = fn;
+  }
+  
+  public emit (event: string, args: any) {
+
+    return this.listeners[event].length > 0
+      ? (this.listeners[event] as Function[]).forEach(f => f(args))
+      : false;
+  }
+
+  public on (event: string, fn: Function) {
+
+    this.listeners[event] = this.listeners[event] || [];
+
+    (this.listeners[event] as Function[]).push(fn);
+  }
+
+  public detach (event: string, fn: Function) {
+
+    const index = (this.listeners[event] as Function[]).indexOf(fn);
+      
+    return index > -1
+      ? (this.listeners[event] as Function[]).splice(index, 1)
+      : false;
+  }
+}
 
 export class Http extends HttpEmitter {
 
@@ -16,12 +137,12 @@ export class Http extends HttpEmitter {
     super();
   }
 
-  setBaseUrl (url: string): void {
+  public setBaseUrl (url: string): void {
 
     this.baseUrl = url;
   }
 
-  getBaseUrl (extention?: string): string {
+  public getBaseUrl (extention?: string): string {
 
     if (extention != undefined) {
 
@@ -33,19 +154,17 @@ export class Http extends HttpEmitter {
     return this.baseUrl;
   }
 
-  setDefaultContentType (type: string): void {
+  public setDefaultContentType (type: string): void {
 
     this.defaultContentType = type;
   }
 
-  getDefaultContentType (): string {
+  public getDefaultContentType (): string {
 
     return this.defaultContentType;
   }
 
-  async makeRequest (request: Omit<HTTPRequestInit, 'params'>): Promise<HTTPResponseObject> {
-    
-    const {url, ...rest} = request as Omit<HTTPRequestInit, 'params'>;
+  public async makeRequest ({url, ...rest}: Omit<HTTPRequestInit, 'params'>): Promise<HTTPResponseObject> {
 
     try {
       
@@ -65,17 +184,17 @@ export class Http extends HttpEmitter {
     } 
   }
 
-  async get (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
+  public async get (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
     
     return await this.makeRequest(new HttpRequestInit(this.listeners, url, {...args, method: 'GET'}));
   }
 
-  async post (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
+  public async post (url: string, args = {} as HTTPRequestObject): Promise<HTTPResponseObject> {
   
     return await this.makeRequest(new HttpRequestInit(this.listeners, url, {...args, method: 'POST'}));
   }
 
-  static getInstance (): Http {
+  public static getInstance (): Http {
 
     return !Http.instance
       ? Http.instance = new Http()
